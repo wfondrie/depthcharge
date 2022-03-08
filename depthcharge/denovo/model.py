f9abc08bafe23c7a258f6557a6ea14d89853073d
William Fondrie <fondriew@gmail.com>
2022-03-08 14:50:54 -0800

    Add beam search decoding

--- depthcharge/denovo/model.py
+++ depthcharge/denovo/model.py
@@ -132,15 +132,21 @@ class Spec2Pep(pl.LightningModule, ModelMixin):
 
         Returns
         -------
-        sequences : list or str
-            The sequence for each spectrum.
-        scores : torch.Tensor of shape (n_spectra, length, n_amino_acids)
+        sequences : list of list of str
+            The top sequences for each spectrum.
+        scores : torch.Tensor of shape
+        (n_spectra, length, n_amino_acids, n_beams)
             The score for each amino acid.
         """
         spectra = spectra.to(self.encoder.device)
         precursors = precursors.to(self.decoder.device)
-        scores, tokens = self.greedy_decode(spectra, precursors)
-        sequences = [self.decoder.detokenize(t) for t in tokens]
+        scores, tokens = self.beam_search_decode(spectra, precursors)
+        sequences = []
+        for spec_tokens in tokens:
+            sequences.append(
+                [self.decoder.detokenize(t) for t in spec_tokens.T]
+            )
+
         return sequences, scores
 
     def predict_step(self, batch, *args):
@@ -190,90 +196,101 @@ class Spec2Pep(pl.LightningModule, ModelMixin):
         """
         memories, mem_masks = self.encoder(spectra)
 
-        # initialize scores:
-        scores = torch.zeros(
-            spectra.shape[0],
-            self.max_length + 1,
-            self.decoder.vocab_size + 1,
-            self.beam_size,
-        )
+        # Sizes:
+        batch = spectra.shape[0]  # B
+        length = self.max_length + 1  # L
+        vocab = self.decoder.vocab_size + 1  # V
+        beam = self.beam_size  # S
+
+        # initialize scores and tokens:
+        scores = torch.zeros(batch, length, vocab, beam)
         scores = scores.type_as(spectra)
+        scores[scores == 0] = torch.nan
+        tokens = torch.zeros(batch, length, beam)
+        tokens = tokens.type_as(spectra).long()
 
         # Create the first prediction:
-        first_pred, _ = self.decoder(None, precursors, memories, mem_masks)
+        pred, _ = self.decoder(None, precursors, memories, mem_masks)
+        _, idx = torch.topk(pred[:, 0, :], beam, dim=1)
+        tokens[:, 0, :] = idx
+        scores[:, :1, :, :] = einops.repeat(pred, "B L V -> B L V S", S=beam)
 
-        # scores is (batch_size, max_length + 1, vocab_size + 1, beam_size)
-        scores[:, :1, :, :] = torch.tile(first_pred, (self.beam_size,))
+        # Make precursors and memories the right shape for decoding:
+        precursors = einops.repeat(precursors, "B L -> (B S) L", S=beam)
+        mem_masks = einops.repeat(mem_masks, "B L -> (B S) L", S=beam)
+        memories = einops.repeat(memories, "B L V -> (B S) L V", S=beam)
 
-        # tokens is (batch_size, max_length, beam_size, beam)
-        tokens = torch.argsort(scores, axis=2, descending=True)
-        tokens = tokens[:, :, : self.beam_size, :]
-
-        for idx in range(2, self.max_length + 2):
+        # The main decoding loop:
+        for idx in range(1, self.max_length + 1):
+            scores = einops.rearrange(scores, "B L V S -> (B S) L V")
+            tokens = einops.rearrange(tokens, "B L S -> (B S) L")
             decoded = (tokens == self.stop_token).any(axis=1)
             if decoded.all():
+                scores = einops.rearrange(
+                    scores,
+                    "(B S) L V -> B L V S",
+                    S=beam,
+                )
+                tokens = einops.rearrange(tokens, "(B S) L -> B L S", S=beam)
                 break
 
-            previous = einops.rearrange(
-                tokens[:, : (idx - 1), :, :],
-            )
-
-    def greedy_decode(self, spectra, precursors):
-        """Greedy decode the spectra.
-
-        Parameters
-        ----------
-        spectrum : torch.Tensor of shape (n_spectra, n_peaks, 2)
-            The spectra to embed. Axis 0 represents a mass spectrum, axis 1
-            contains the peaks in the mass spectrum, and axis 2 is essentially
-            a 2-tuple specifying the m/z-intensity pair for each peak. These
-            should be zero-padded, such that all of the spectra in the batch
-            are the same length.
-        precursors : torch.Tensor of size (n_spectra, 2)
-            The measured precursor mass (axis 0) and charge (axis 1) of each
-            tandem mass spectrum.
-
-        Returns
-        -------
-        tokens : torch.Tensor of shape (n_spectra, max_length, n_amino_acids)
-            The token sequence for each spectrum.
-        scores : torch.Tensor of shape (n_spectra, max_length, n_amino_acids)
-            The score for each amino acid.
-        """
-        memories, mem_masks = self.encoder(spectra)
-
-        # initialize scores:
-        scores = torch.zeros(
-            spectra.shape[0],
-            self.max_length + 1,
-            self.decoder.vocab_size + 1,
-        )
-        scores = scores.type_as(spectra)
-
-        # The first prediction:
-        scores[:, :1, :], _ = self.decoder(
-            None,
-            precursors,
-            memories,
-            mem_masks,
-        )
-
-        tokens = torch.argmax(scores, axis=2)
-
-        # Keep predicting until all have a stop token or max_length is reached.
-        # Don't count the stop token toward max_length though.
-        for idx in range(2, self.max_length + 2):
-            decoded = (tokens == self.stop_token).any(axis=1)
-            if decoded.all():
-                break
-
-            scores[~decoded, :idx, :], _ = self.decoder(
-                tokens[~decoded, : (idx - 1)],
+            # Get scores:
+            scores[~decoded, : idx + 1, :], _ = self.decoder(
+                tokens[~decoded, :idx],
                 precursors[~decoded, :],
                 memories[~decoded, :, :],
                 mem_masks[~decoded, :],
             )
-            tokens = torch.argmax(scores, axis=2)
+
+            # Reshape to group by spectrum (B for "batch")
+            scores = einops.rearrange(scores, "(B S) L V -> B L V S", S=beam)
+            tokens = einops.rearrange(tokens, "(B S) L -> B L S", S=beam)
+            prev_tokens = einops.repeat(
+                tokens[:, :idx, :],
+                "B L S -> B L V S",
+                V=vocab,
+            )
+
+            # Get the previous tokens and scores:
+            prev_scores = torch.gather(
+                scores[:, :idx, :, :],
+                dim=2,
+                index=prev_tokens,
+            )
+            prev_scores = einops.repeat(
+                prev_scores[:, :, 0, :],
+                "B L S -> B L (V S)",
+                V=vocab,
+            )
+
+            # Figure out the top K decodings:
+            step_scores = torch.zeros(batch, idx + 1, beam * vocab)
+            step_scores = step_scores.type_as(scores)
+            step_scores[:, :idx, :] = prev_scores
+            step_scores[:, idx, :] = einops.rearrange(
+                scores[:, idx, :, :],
+                "B V S -> B (V S)",
+            )
+
+            _, top_idx = torch.topk(step_scores.nanmean(dim=1), beam)
+            V_idx, S_idx = np.unravel_index(top_idx, (vocab, beam))
+
+            S_idx = einops.rearrange(S_idx, "B S -> (B S)")
+            # V_idx = einops.rearrange(V_idx, "B S -> (B S)")
+            B_idx = einops.repeat(torch.arange(batch), "B -> (B S)", S=beam)
+
+            # Record the top K decodings
+            tokens[:, :idx, :] = einops.rearrange(
+                prev_tokens[B_idx, :, 0, S_idx],
+                "(B S) L -> B L S",
+                S=beam,
+            )
+            tokens[:, idx, :] = torch.tensor(V_idx)
+            scores[:, : idx + 1, :, :] = einops.rearrange(
+                scores[B_idx, : idx + 1, :, S_idx],
+                "(B S) L V -> B L V S",
+                S=beam,
+            )
 
         return self.softmax(scores), tokens
 
