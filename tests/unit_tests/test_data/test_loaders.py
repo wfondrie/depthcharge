21aed359dd41f94421742a58d9e47ee88159cc3e
William Fondrie <fondriew@gmail.com>
2022-05-04 12:17:43 -0700

    Add test for fewer peaks

--- tests/unit_tests/test_data/test_loaders.py
+++ tests/unit_tests/test_data/test_loaders.py
@@ -28,6 +28,27 @@ def test_spectrum_index_init(mgf_small, tmp_path):
     assert batch[0].shape[2] == 2
     assert batch[1].shape == (1, 2)
 
+    data_module = SpectrumDataModule(
+        train_index=index,
+        val_index=index,
+        test_index=index,
+        batch_size=1,
+        num_workers=0,
+        n_peaks=3,
+    )
+
+    data_module.setup()
+    assert data_module.train_dataloader is not None
+    assert data_module.val_dataloader is not None
+    assert data_module.test_dataloader is not None
+
+    batch = next(iter(data_module.train_dataloader()))
+    assert len(batch) == 2
+    assert batch[0].shape[0] == 1
+    assert batch[0].shape[1] == 3
+    assert batch[0].shape[2] == 2
+    assert batch[1].shape == (1, 2)
+
 
 def test_ann_spectrum_index_init(mgf_small, tmp_path):
     """Test initialization of with a SpectrumIndex"""
