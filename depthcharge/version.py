14354060a7864a2977b12c9f28a2a0e1762e3caf
William Fondrie <fondriew@gmail.com>
2023-05-15 22:37:39 -0700

    Improved test coverage

--- depthcharge/version.py
+++ depthcharge/version.py
@@ -1,19 +1,9 @@
-"""Get the package version"""
+"""Get the version information."""
 from importlib.metadata import version, PackageNotFoundError
 
 
-def _get_version():
-    """Retreive the package version.
-
-    Using setuptools-scm dynamically sets the pacakge version depending on
-    the latest git release and commits since that point. This function
-    returns the current version number.
-
-    Returns
-    -------
-    str or None
-        The package version number. If not version is found, returns None.
-    """
+def _get_version() -> str:
+    """Return the version information for deptcharge."""
     try:
         return version("depthcharge-ms")
     except PackageNotFoundError:
