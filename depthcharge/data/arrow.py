0ff64ccc9a4ef24e495fec52953e53906d6aa3be
William Fondrie <fondriew@gmail.com>
2023-09-25 16:08:40 -0700

    Migrating to arrow backend

---
+++ depthcharge/data/arrow.py
@@ -0,0 +1,281 @@
+"""Store spectrum data as Arrow tables."""
+from os import PathLike
+from pathlib import Path
+from typing import Callable, Iterable, Generator
+
+import polars as pl
+import pyarrow as pa
+import pyarrow.parquet as pq
+
+from .parsers import ParserFactory
+
+
+def stream_spectra(
+    peak_file: PathLike,
+    batch_size: int | None,
+    *,
+    metadata_df: pl.DataFrame | None = None,
+    ms_level: int | Iterable[int] | None = 2,
+    preprocessing_fn: Callable | Iterable[Callable] | None = None,
+    valid_charge: Iterable[int] | None = None,
+    custom_fields: dict[str, Iterable[str | int]] = None,
+) -> Generator[pa.RecordBatch]:
+    """Stream mass spectra in an Apache Arrow format, with preprocessing.
+
+    Apache Arrow is a space efficient, columnar data format
+    that is popular in the data science and engineering community.
+    This function reads data from a mass spectrometry data format,
+    extracts the mass spectrum and identifying information. By
+    default, the schema is:
+        scan_id: int
+        ms_level: int
+        precursor_mz: float
+        precursor_charge: int
+        mz_array: list[float]
+        intensity_array: list[float]
+
+    An optional metadata DataFrame can be provided to add additional
+    metadata to each mass spectrum. This DataFrame must contain
+    a ``scan_id`` column containing the integer scan identifier for
+    each mass spectrum. For mzML files, this is generally the integer
+    following ``scan=``, whereas for MGF files this is the zero-indexed
+    offset of the mass spectrum in the file.
+
+    Finally, custom fields can be extracted from the mass spectrometry
+    data file for advanced use. A dictionary providing the key for
+    each spectrum returned by the corresponding pyteomics parser can
+    be provided, along with a column name for the value.
+
+    Parameters
+    ----------
+    peak_file : PathLike, optional
+        The mass spectrometry data file in mzML, mzXML, or MGF format.
+    batch_size : int or None
+        The number of mass spectra in each RecordBatch. ``None`` will load
+        all of the spectra in a single batch.
+    metadata_df : DataFrame, optional
+        A `polars.DataFrame` or `pandas.DataFrame` containing additional
+        metadata from the spectra. This is merged on the `scan_id` column
+        which must be present.
+    ms_level : int, list of int, or None, optional
+        The level(s) of tandem mass spectra to keep. `None` will retain
+        all spectra.
+    preprocessing_fn : Callable or Iterable[Callable], optional
+        The function(s) used to preprocess the mass spectra. `None`,
+        the default, filters for the top 200 peaks above m/z 140,
+        square root transforms the intensities and scales them to unit norm.
+        See the preprocessing module for details and additional options.
+    valid_charge : int or list of int, optional
+        Only consider spectra with the specified precursor charges. If `None`,
+        any precursor charge is accepted.
+    custom_fields : dict of str to list of str, optional
+        Additional field to extract during peak file parsing. The key must
+        be the resulting column name and value must be an interable of
+        containing the necessary keys to retreive the value from the
+        spectrum from the corresponding Pyteomics parser.
+
+    Returns
+    -------
+    Generator of pyarrow.RecordBatch
+        Batches of parsed spectra.
+    """
+    parser_args = {
+        "ms_level": ms_level,
+        "valid_charge": valid_charge,
+        "preprocessing_fn": preprocessing_fn,
+        "custom_fields": custom_fields
+    }
+
+    metadata_df = None if metadata_df is None else metadata_df.lazy()
+    batch_size = float("inf") if batch_size is None else batch_size
+
+    parser = ParserFactory.get_parser(peak_file, **parser_args)
+    for batch in parser.iter_batches(batch_size=batch_size):
+        if metadata_df is not None:
+            batch = (
+                pl.LazyFrame(batch)
+                .join(
+                    metadata_df,
+                    on="scan_id",
+                    how="left",
+                    validate="1:1",
+                )
+                .collect()
+                .to_arrow()
+                .to_batches(max_size=batch_size)[0]
+            )
+
+        yield batch
+
+
+def spectra_to_parquet(
+    peak_file: PathLike,
+    *,
+    parquet_file: PathLike = None,
+    batch_size: int = 100_000,
+    metadata_df: pl.DataFrame | None = None,
+    ms_level: int | Iterable[int] | None = 2,
+    preprocessing_fn: Callable | Iterable[Callable] | None = None,
+    valid_charge: Iterable[int] | None = None,
+    custom_fields: dict[str, Iterable[str, int]] = None,
+) -> Path:
+    """Stream mass spectra to Apache Parquet, with preprocessing.
+
+    Apache Parquet is a space efficient, columnar data storage format
+    that is popular in the data science and engineering community.
+    This function reads data from a mass spectrometry data format,
+    extracts the mass spectrum and identifying information. By
+    default, the schema is:
+        scan_id: int
+        ms_level: int
+        precursor_mz: float64
+        precursor_charge: int8
+        mz_array: list[float64]
+        intensity_array: list[float64]
+
+    An optional metadata DataFrame can be provided to add additional
+    metadata to each mass spectrum. This DataFrame must contain
+    a ``scan_id`` column containing the integer scan identifier for
+    each mass spectrum. For mzML files, this is generally the integer
+    following ``scan=``, whereas for MGF files this is the zero-indexed
+    offset of the mass spectrum in the file.
+
+    Finally, custom fields can be extracted from the mass spectrometry
+    data file for advanced use. A dictionary providing the key for
+    each spectrum returned by the corresponding pyteomics parser can
+    be provided, along with a column name for the value.
+
+    Parameters
+    ----------
+    peak_file : PathLike, optional
+        The mass spectrometry data file in mzML, mzXML, or MGF format.
+    parquet_file : PathLike, optional
+        The output file. By default this is the input file stem with a
+        `.parquet` extension.
+    batch_size : int
+        The number of mass spectra to process simultaneously.
+    metadata_df : DataFrame, optional
+        A `polars.DataFrame` or `pandas.DataFrame` containing additional
+        metadata from the spectra. This is merged on the `scan_id` column
+        which must be present.
+    ms_level : int, list of int, or None, optional
+        The level(s) of tandem mass spectra to keep. `None` will retain
+        all spectra.
+    preprocessing_fn : Callable or Iterable[Callable], optional
+        The function(s) used to preprocess the mass spectra. `None`,
+        the default, filters for the top 200 peaks above m/z 140,
+        square root transforms the intensities and scales them to unit norm.
+        See the preprocessing module for details and additional options.
+    valid_charge : int or list of int, optional
+        Only consider spectra with the specified precursor charges. If `None`,
+        any precursor charge is accepted.
+    custom_fields : dict of str to list of str, optional
+        Additional field to extract during peak file parsing. The key must
+        be the resulting column name and value must be an interable of
+        containing the necessary keys to retreive the value from the
+        spectrum from the corresponding Pyteomics parser.
+
+    Returns
+    -------
+    Path
+        The Parquet file that was written.
+    """
+    streamer = stream_spectra(
+        peak_file=peak_file,
+        batch_size=batch_size,
+        metadata_df=metadata_df,
+        ms_level=ms_level,
+        preprocessing_fn=preprocessing_fn,
+        valid_charge=valid_charge,
+        custom_fields=custom_fields,
+    )
+
+    if parquet_file is None:
+        parquet_file = Path(Path(peak_file).stem).with_suffix(".parquet")
+
+    writer = None
+    for batch in streamer:
+        if writer is None:
+            writer = pq.ParquetWriter(parquet_file, schema=batch.schema)
+
+        writer.write_batch(batch)
+
+    return parquet_file
+
+
+def spectra_to_df(
+    peak_file: PathLike,
+    *,
+    metadata_df: pl.DataFrame | None = None,
+    ms_level: int | Iterable[int] | None = 2,
+    preprocessing_fn: Callable | Iterable[Callable] | None = None,
+    valid_charge: Iterable[int] | None = None,
+    custom_fields: dict[str, Iterable[str, int]] = None,
+) -> pl.DataFrame:
+    """Read mass spectra into a Polars DataFrame.
+
+    Apache Parquet is a space efficient, columnar data storage format
+    that is popular in the data science and engineering community.
+    This function reads data from a mass spectrometry data format,
+    extracts the mass spectrum and identifying information. By
+    default, the schema is:
+        scan_id: int
+        ms_level: int
+        precursor_mz: float64
+        precursor_charge: int8
+        mz_array: list[float64]
+        intensity_array: list[float64]
+
+    An optional metadata DataFrame can be provided to add additional
+    metadata to each mass spectrum. This DataFrame must contain
+    a ``scan_id`` column containing the integer scan identifier for
+    each mass spectrum. For mzML files, this is generally the integer
+    following ``scan=``, whereas for MGF files this is the zero-indexed
+    offset of the mass spectrum in the file.
+
+    Finally, custom fields can be extracted from the mass spectrometry
+    data file for advanced use. A dictionary providing the key for
+    each spectrum returned by the corresponding pyteomics parser can
+    be provided, along with a column name for the value.
+
+    Parameters
+    ----------
+    peak_file : PathLike, optional
+        The mass spectrometry data file in mzML, mzXML, or MGF format.
+    metadata_df : DataFrame, optional
+        A `polars.DataFrame` or `pandas.DataFrame` containing additional
+        metadata from the spectra. This is merged on the `scan_id` column
+        which must be present.
+    ms_level : int, list of int, or None, optional
+        The level(s) of tandem mass spectra to keep. `None` will retain
+        all spectra.
+    preprocessing_fn : Callable or Iterable[Callable], optional
+        The function(s) used to preprocess the mass spectra. `None`,
+        the default, filters for the top 200 peaks above m/z 140,
+        square root transforms the intensities and scales them to unit norm.
+        See the preprocessing module for details and additional options.
+    valid_charge : int or list of int, optional
+        Only consider spectra with the specified precursor charges. If `None`,
+        any precursor charge is accepted.
+    custom_fields : dict of str to list of str, optional
+        Additional field to extract during peak file parsing. The key must
+        be the resulting column name and value must be an interable of
+        containing the necessary keys to retreive the value from the
+        spectrum from the corresponding Pyteomics parser.
+
+    Returns
+    -------
+    Path
+        The Parquet file that was written.
+    """
+    streamer = stream_spectra(
+        peak_file=peak_file,
+        batch_size=float("inf"),
+        metadata_df=metadata_df,
+        ms_level=ms_level,
+        preprocessing_fn=preprocessing_fn,
+        valid_charge=valid_charge,
+        custom_fields=custom_fields,
+    )
+
+    return pl.DataFrame(streamer)
--- depthcharge/data/parsers.py
+++ depthcharge/data/parsers.py
@@ -6,8 +6,9 @@ from abc import ABC, abstractmethod
 from collections.abc import Callable, Iterable
 from os import PathLike
 from pathlib import Path
+from typing import Any
 
-import numpy as np
+import pyarrow as pa
 from pyteomics.mgf import MGF
 from pyteomics.mzml import MzML
 from pyteomics.mzxml import MzXML
@@ -24,7 +25,7 @@ class BaseParser(ABC):
 
     Parameters
     ----------
-    ms_data_file : PathLike
+    peak_file : PathLike
         The peak file to parse.
     ms_level : int
         The MS level of the spectra to parse.
@@ -33,38 +34,53 @@ class BaseParser(ABC):
     valid_charge : Iterable[int], optional
         Only consider spectra with the specified precursor charges. If `None`,
         any precursor charge is accepted.
+    custom_fields : dict of str to list of str, optional
+        Additional field to extract during peak file parsing. The key must
+        be the resulting column name and value must be an interable of
+        containing the necessary keys to retreive the value from the
+        spectrum from the corresponding Pyteomics parser.
     id_type : str, optional
         The Hupo-PSI prefix for the spectrum identifier.
     """
-
     def __init__(
         self,
-        ms_data_file: PathLike,
-        ms_level: int,
+        peak_file: PathLike,
+        ms_level: int | Iterable[int] | None = 2,
         preprocessing_fn: Callable | Iterable[Callable] | None = None,
         valid_charge: Iterable[int] | None = None,
+        custom_fields: dict[str, str | Iterable[str]] | None = None,
         id_type: str = "scan",
     ) -> None:
         """Initialize the BaseParser."""
-        self.path = Path(ms_data_file)
-        self.ms_level = ms_level
+        self.peak_file = Path(peak_file)
+        self.ms_level = (
+            ms_level if ms_level is None else set(utils.listify(ms_level))
+        )
+
         if preprocessing_fn is None:
             self.preprocessing_fn = []
         else:
             self.preprocessing_fn = utils.listify(preprocessing_fn)
 
         self.valid_charge = None if valid_charge is None else set(valid_charge)
+        self.custom_fields = custom_fields
         self.id_type = id_type
-        self.offset = None
-        self.precursor_mz = []
-        self.precursor_charge = []
-        self.scan_id = []
-        self.mz_arrays = []
-        self.intensity_arrays = []
-        self.annotations = None
+
+        # Check format:
+        self.sniff()
 
     @abstractmethod
-    def open(self) -> Iterable:
+    def sniff(self) -> None:
+        """Quickly test a file for the correct type.
+
+        Raises
+        ------
+        IOError
+            Raised if the file is not the expected format.
+        """
+
+    @abstractmethod
+    def open(self) -> Iterable[dict]:
         """Open the file as an iterable."""
 
     @abstractmethod
@@ -82,66 +98,97 @@ class BaseParser(ABC):
             The parsed mass spectrum or None if it is skipped.
         """
 
-    def read(self) -> BaseParser:
-        """Read the ms data file.
+    def parse_custom_fields(self, spectrum: dict) -> dict[str, Any]:
+        """Parse user-provided fields.
+
+        Parameters
+        ----------
+        spectrum : dict
+            The dictionary defining the spectrum in a given format.
 
         Returns
         -------
-        Self
+        dict
+            The parsed value of each, whatever it may be.
+        """
+        out = {}
+        if self.custom_fields is None:
+            return out
+
+        for field, accessors in self.custom_fields.items():
+            val = spectrum
+            for accessor in accessors:
+                val = val[accessor]
+
+            out[field] = val
+
+        return out
+
+    def iter_batches(self, batch_size: int) -> pa.RecordBatch:
+        """Iterate over batches of mass spectra in the Arrow format.
+
+        Parameters
+        ----------
+        batch_size : int
+            The number of spectra in a batch.
+
+        Yields
+        -------
+        RecordBatch
+            A batch of spectra and their metadata.
         """
         n_skipped = 0
+        metadata = {
+            "peak_file": self.peak_file.name,
+            "id_type": self.id_type,
+        }
+
+        pbar_args = {
+            "desc": self.peak_file.name,
+            "unit": " spectra",
+        }
+
         with self.open() as spectra:
-            for spectrum in tqdm(spectra, desc=str(self.path), unit="spectra"):
+            batch = []
+            for spectrum in tqdm(spectra, **pbar_args):
                 try:
-                    spectrum = self.parse_spectrum(spectrum)
+                    parsed = self.parse_spectrum(spectrum)
                     if spectrum is None:
                         continue
 
                     if self.preprocessing_fn is not None:
                         for processor in self.preprocessing_fn:
-                            spectrum = processor(spectrum)
+                            parsed = processor(parsed)
 
-                    self.mz_arrays.append(spectrum.mz)
-                    self.intensity_arrays.append(spectrum.intensity)
-                    self.precursor_mz.append(spectrum.precursor_mz)
-                    self.precursor_charge.append(spectrum.precursor_charge)
-                    self.scan_id.append(_parse_scan_id(spectrum.scan_id))
-                    if self.annotations is not None:
-                        self.annotations.append(spectrum.label)
+                    entry = {
+                        "scan_id": _parse_scan_id(parsed.scan_id),
+                        "ms_level": parsed.ms_level,
+                        "precursor_mz": parsed.precursor_mz,
+                        "precursor_charge": parsed.precursor_charge,
+                        "mz_array": parsed.mz,
+                        "intensity_array": parsed.intensity,
+                    }
+
                 except (IndexError, KeyError, ValueError):
                     n_skipped += 1
+                    continue
+
+                # Parse custom fields:
+                entry.update(self.parse_custom_fields(spectrum))
+                batch.append(entry)
+
+                if len(batch) >= batch_size:
+                    yield pa.RecordBatch.from_pylist(batch, metadata=metadata)
+                    batch = []
+
+            # Get the remainder:
+            if batch:
+                yield pa.RecordBatch.from_pylist(batch, metadata=metadata)
 
         if n_skipped:
             LOGGER.warning(
-                "Skipped %d spectra with invalid precursor info", n_skipped
+                "Skipped %d spectra with invalid information", n_skipped
             )
-
-        self.precursor_mz = np.array(self.precursor_mz, dtype=np.float64)
-        self.precursor_charge = np.array(
-            self.precursor_charge,
-            dtype=np.uint8,
-        )
-
-        self.scan_id = np.array(self.scan_id)
-
-        # Build the index
-        sizes = np.array([0] + [s.shape[0] for s in self.mz_arrays])
-        self.offset = sizes[:-1].cumsum()
-        self.mz_arrays = np.concatenate(self.mz_arrays).astype(np.float64)
-        self.intensity_arrays = np.concatenate(self.intensity_arrays).astype(
-            np.float32
-        )
-        return self
-
-    @property
-    def n_spectra(self) -> int:
-        """The number of spectra."""
-        return self.offset.shape[0]
-
-    @property
-    def n_peaks(self) -> int:
-        """The number of peaks in the file."""
-        return self.mz_arrays.shape[0]
 
 
 class MzmlParser(BaseParser):
@@ -149,7 +196,7 @@ class MzmlParser(BaseParser):
 
     Parameters
     ----------
-    ms_data_file : PathLike
+    peak_file : PathLike
         The mzML file to parse.
     ms_level : int
         The MS level of the spectra to parse.
@@ -159,10 +206,22 @@ class MzmlParser(BaseParser):
         Only consider spectra with the specified precursor charges. If `None`,
         any precursor charge is accepted.
     """
+    def sniff(self) -> None:
+        """Quickly test a file for the correct type.
+
+        Raises
+        ------
+        IOError
+            Raised if the file is not the expected format.
+        """
+        with self.peak_file.open() as mzdat:
+            next(mzdat)
+            if "http://psi.hupo.org/ms/mzml" not in next(mzdat):
+                raise IOError("Not an mzML file.")
 
     def open(self) -> Iterable[dict]:
         """Open the mzML file for reading."""
-        return MzML(str(self.path))
+        return MzML(str(self.peak_file))
 
     def parse_spectrum(self, spectrum: dict) -> MassSpectrum | None:
         """Parse a single spectrum.
@@ -177,10 +236,11 @@ class MzmlParser(BaseParser):
         MassSpectrum or None
             The parsed mass spectrum or None if not at the correct MS level.
         """
-        if spectrum["ms level"] != self.ms_level:
+        ms_level = spectrum["ms level"]
+        if self.ms_level is not None and ms_level not in self.ms_level:
             return None
 
-        if self.ms_level > 1:
+        if ms_level > 1:
             precursor = spectrum["precursorList"]["precursor"][0]
             precursor_ion = precursor["selectedIonList"]["selectedIon"][0]
             precursor_mz = float(precursor_ion["selected ion m/z"])
@@ -195,15 +255,16 @@ class MzmlParser(BaseParser):
 
         if self.valid_charge is None or precursor_charge in self.valid_charge:
             return MassSpectrum(
-                filename=str(self.path),
+                filename=str(self.peak_file),
                 scan_id=spectrum["id"],
                 mz=spectrum["m/z array"],
                 intensity=spectrum["intensity array"],
+                ms_level=ms_level,
                 precursor_mz=precursor_mz,
                 precursor_charge=precursor_charge,
             )
 
-        raise ValueError("Invalid precursor charge")
+        raise ValueError("Invalid precursor charge.")
 
 
 class MzxmlParser(BaseParser):
@@ -211,7 +272,7 @@ class MzxmlParser(BaseParser):
 
     Parameters
     ----------
-    ms_data_file : PathLike
+    peak_file : PathLike
         The mzXML file to parse.
     ms_level : int
         The MS level of the spectra to parse.
@@ -221,10 +282,23 @@ class MzxmlParser(BaseParser):
         Only consider spectra with the specified precursor charges. If `None`,
         any precursor charge is accepted.
     """
+    def sniff(self) -> None:
+        """Quickly test a file for the correct type.
+
+        Raises
+        ------
+        IOError
+            Raised if the file is not the expected format.
+        """
+        scent = "http://sashimi.sourceforge.net/schema_revision/mzXML"
+        with self.peak_file.open() as mzdat:
+            next(mzdat)
+            if scent not in next(mzdat):
+                raise IOError("Not an mzXML file.")
 
     def open(self) -> Iterable[dict]:
         """Open the mzXML file for reading."""
-        return MzXML(str(self.path))
+        return MzXML(str(self.peak_file))
 
     def parse_spectrum(self, spectrum: dict) -> MassSpectrum | None:
         """Parse a single spectrum.
@@ -239,10 +313,11 @@ class MzxmlParser(BaseParser):
         MassSpectrum
             The parsed mass spectrum.
         """
-        if spectrum["msLevel"] != self.ms_level:
+        ms_level = spectrum["msLevel"]
+        if self.ms_level is not None and ms_level not in self.ms_level:
             return None
 
-        if self.ms_level > 1:
+        if ms_level > 1:
             precursor = spectrum["precursorMz"][0]
             precursor_mz = float(precursor["precursorMz"])
             precursor_charge = int(precursor.get("precursorCharge", 0))
@@ -251,10 +326,11 @@ class MzxmlParser(BaseParser):
 
         if self.valid_charge is None or precursor_charge in self.valid_charge:
             return MassSpectrum(
-                filename=str(self.path),
+                filename=str(self.peak_file),
                 scan_id=spectrum["id"],
                 mz=spectrum["m/z array"],
                 intensity=spectrum["intensity array"],
+                ms_level=ms_level,
                 precursor_mz=precursor_mz,
                 precursor_charge=precursor_charge,
             )
@@ -267,7 +343,7 @@ class MgfParser(BaseParser):
 
     Parameters
     ----------
-    ms_data_file : PathLike
+    peak_file : PathLike
         The MGF file to parse.
     ms_level : int
         The MS level of the spectra to parse.
@@ -276,34 +352,41 @@ class MgfParser(BaseParser):
     valid_charge : Iterable[int], optional
         Only consider spectra with the specified precursor charges. If `None`,
         any precursor charge is accepted.
-    annotations : bool
-        Include peptide annotations.
     """
 
     def __init__(
         self,
-        ms_data_file: PathLike,
+        peak_file: PathLike,
         ms_level: int = 2,
         preprocessing_fn: Callable | Iterable[Callable] | None = None,
         valid_charge: Iterable[int] | None = None,
-        annotations: bool = False,
     ) -> None:
         """Initialize the MgfParser."""
         super().__init__(
-            ms_data_file,
+            peak_file,
             ms_level=ms_level,
             preprocessing_fn=preprocessing_fn,
             valid_charge=valid_charge,
             id_type="index",
         )
-        if annotations:
-            self.annotations = []
-
         self._counter = -1
+        self._assumed_ms_level = sorted(list(self.ms_level))[0]
+
+    def sniff(self) -> None:
+        """Quickly test a file for the correct type.
+
+        Raises
+        ------
+        IOError
+            Raised if the file is not the expected format.
+        """
+        with self.peak_file.open() as mzdat:
+            if not next(mzdat).startswith("BEGIN IONS"):
+                raise IOError("Not an MGF file.")
 
     def open(self) -> Iterable[dict]:
         """Open the MGF file for reading."""
-        return MGF(str(self.path))
+        return MGF(str(self.peak_file))
 
     def parse_spectrum(self, spectrum: dict) -> MassSpectrum:
         """Parse a single spectrum.
@@ -314,30 +397,24 @@ class MgfParser(BaseParser):
             The dictionary defining the spectrum in MGF format.
         """
         self._counter += 1
-
-        if self.ms_level > 1:
+        if self.ms_level is not None and 1 not in self.ms_level:
             precursor_mz = float(spectrum["params"]["pepmass"][0])
             precursor_charge = int(spectrum["params"].get("charge", [0])[0])
         else:
             precursor_mz, precursor_charge = None, 0
 
-        if self.annotations is not None:
-            label = spectrum["params"].get("seq")
-        else:
-            label = None
-
         if self.valid_charge is None or precursor_charge in self.valid_charge:
             return MassSpectrum(
-                filename=str(self.path),
+                filename=str(self.peak_file),
                 scan_id=self._counter,
                 mz=spectrum["m/z array"],
                 intensity=spectrum["intensity array"],
+                ms_level=self._assumed_ms_level,
                 precursor_mz=precursor_mz,
                 precursor_charge=precursor_charge,
-                label=label,
             )
 
-        raise ValueError("Invalid precursor charge")
+        raise ValueError("Invalid precursor charge.")
 
 
 def _parse_scan_id(scan_str: str | int) -> int:
@@ -367,3 +444,29 @@ def _parse_scan_id(scan_str: str | int) -> int:
             pass
 
     raise ValueError("Failed to parse scan number")
+
+
+class ParserFactory:
+    """Figure out what parser to use."""
+    parsers = [
+        MzmlParser,
+        MzxmlParser,
+        MgfParser,
+    ]
+
+    @classmethod
+    def get_parser(cls, peak_file: PathLike, **kwargs) -> BaseParser:
+        """Get the correct parser for a peak file.
+
+        Parameters
+        ----------
+        peak_file: PathLike
+            The peak file to parse.
+        """
+        for parser in cls.parsers:
+            try:
+                return parser(peak_file)
+            except IOError:
+                pass
+
+        raise IOError("Unknown file format.")
