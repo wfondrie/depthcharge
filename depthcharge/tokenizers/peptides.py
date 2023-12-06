Various fixes and new precursor calculation

depthcharge/tokenizers/peptides.py
@@ -244,6 +244,10 @@ class PeptideIonTokenizer(PeptideTokenizer):
             reverse=reverse,
         )
 
+        self.masses = torch.tensor(
+            [0.0] * 2 + [self.residues[a] for a in self.reverse_index[2:]]
+        )
+
     def __getstate__(self) -> dict:
         """How to pickle the object."""
         self.residues = dict(self.residues)
@@ -259,7 +263,32 @@ class PeptideIonTokenizer(PeptideTokenizer):
         )
         self.residues.update(residues)
 
-    def ions(  # noqa: C901
+    def calculate_precursor_ions(
+        self,
+        tokens: torch.Tensor | Iterable[str],
+        charges: torch.Tensor,
+    ) -> torch.Tensor:
+        """Calculate the m/z for the precursor without leaving PyTorch.
+
+        Parameters
+        ----------
+        tokens : torch.Tensor of shape (n_sequences, len_seq)
+            The tokens corresponding to the peptide sequence.
+        charges : torch.Tensor of shape (n_sequences,)
+            The charge state for each peptide.
+
+        Returns
+        -------
+        torch.Tensor
+            The monoisotopic m/z for each charged peptide.
+        """
+        if isinstance(tokens[0], str):
+            tokens = self.tokenize(tokens)
+
+        masses = self.masses[tokens].sum(dim=1) + H2O
+        return (masses / charges) + PROTON
+
+    def calculate_ions(  # noqa: C901
         self,
         sequences: Iterable[str],
         precursor_charges: Iterable[int] | str,

depthcharge/transformers/analytes.py
@@ -105,7 +105,7 @@ class _AnalyteTransformer(torch.nn.Module, ModelMixin, TransformerMixin):
             The precursor representations.
         """
         return torch.zeros((tokens.shape[0], self.d_model)).type_as(
-            self.token_encoder
+            self.token_encoder.weight
         )
 
 
@@ -205,7 +205,7 @@ class AnalyteTransformerEncoder(_AnalyteTransformer):
         # Encode everything:
         encoded = self.token_encoder(tokens)
         global_token = self.global_token_hook(tokens, *args, **kwargs)
-        encoded = torch.cat([global_token, encoded], dim=1)
+        encoded = torch.cat([global_token[:, None, :], encoded], dim=1)
 
         # Create mask
         key_mask = ~encoded.sum(dim=2).bool()
@@ -284,7 +284,7 @@ class AnalyteTransformerDecoder(_AnalyteTransformer):
 
         self.final = torch.nn.Linear(
             d_model,
-            self.aa_encoder.num_embeddings - 1,
+            self.token_encoder.num_embeddings - 1,
         )
 
     def forward(
@@ -308,10 +308,10 @@ class AnalyteTransformerDecoder(_AnalyteTransformer):
         *args : torch.Tensor, optional
             Additional data. These may be used by overwriting the
             `global_token_hook()` method in a subclass.
-        memory : torch.Tensor of shape (batch_size, n_peaks, d_model)
+        memory : torch.Tensor of shape (batch_size, len_seq, d_model)
             The representations from a ``TransformerEncoder``, such as a
             ``SpectrumTransformerEncoder``.
-        memory_key_padding_mask : torch.Tensor of shape (batch_size, n_peaks)
+        memory_key_padding_mask : torch.Tensor of shape (batch_size, len_seq)
             Passed to `torch.nn.TransformerEncoder.forward()`. The mask that
             indicates which elements of ``memory`` are padding.
         memory_mask : torch.Tensor
@@ -340,7 +340,7 @@ class AnalyteTransformerDecoder(_AnalyteTransformer):
         # Encode everything:
         encoded = self.token_encoder(tokens)
         global_token = self.global_token_hook(tokens, *args, **kwargs)
-        encoded = torch.cat([global_token, encoded], dim=1)
+        encoded = torch.cat([global_token[:, None, :], encoded], dim=1)
 
         # Feed through model:
         tgt_key_padding_mask = encoded.sum(axis=2) == 0
@@ -356,7 +356,7 @@ class AnalyteTransformerDecoder(_AnalyteTransformer):
             memory=memory,
             tgt_mask=tgt_mask,
             tgt_key_padding_mask=tgt_key_padding_mask,
-            memory_key_padding_mask=memory_key_padding_mask.to(self.device),
+            memory_key_padding_mask=memory_key_padding_mask,
             memory_mask=memory_mask,
         )
         return self.final(preds)

tests/unit_tests/test_tokenizers/test_peptides.py
@@ -134,29 +134,61 @@ def test_precursor_ions():
     close = partial(math.isclose, rel_tol=1e-6)
 
     seq = "LESLIEK"
-    assert close(tokenizer.ions(seq, 1)[0].precursor, pymass(seq, charge=1))
-    assert close(tokenizer.ions(seq, 2)[0].precursor, pymass(seq, charge=2))
-    assert close(tokenizer.ions(seq, 3)[0].precursor, pymass(seq, charge=3))
+    assert close(
+        tokenizer.calculate_ions(seq, 1)[0].precursor, pymass(seq, charge=1)
+    )
+    assert close(
+        tokenizer.calculate_ions(seq, 2)[0].precursor, pymass(seq, charge=2)
+    )
+    assert close(
+        tokenizer.calculate_ions(seq, 3)[0].precursor, pymass(seq, charge=3)
+    )
 
     seq = "[Acetyl]-LESLIM[Oxidation]K"
     with pytest.raises(ValueError):
-        tokenizer.ions(seq, 1)
+        tokenizer.calculate_ions(seq, 1)
 
     tokenizer = PeptideIonTokenizer.from_proforma([seq])
     seq2 = "aLESLIMoK"
-    assert close(tokenizer.ions(seq, 1)[0].precursor, pymass(seq2, charge=1))
-    assert close(tokenizer.ions(seq, 2)[0].precursor, pymass(seq2, charge=2))
-    assert close(tokenizer.ions(seq, 3)[0].precursor, pymass(seq2, charge=3))
+    assert close(
+        tokenizer.calculate_ions(seq, 1)[0].precursor, pymass(seq2, charge=1)
+    )
+    assert close(
+        tokenizer.calculate_ions(seq, 2)[0].precursor, pymass(seq2, charge=2)
+    )
+    assert close(
+        tokenizer.calculate_ions(seq, 3)[0].precursor, pymass(seq2, charge=3)
+    )
+
+
+def test_torch_precursor_ions():
+    """Test the calculation of the precursor m/z."""
+    seqs = ["LESLIEK", "EDITHR"]
+    charges = torch.tensor([2, 3])
+    tokenizer = PeptideIonTokenizer.from_proforma(seqs)
+    expected = torch.tensor(
+        [
+            mass.fast_mass(s, charge=z, ion_type="M")
+            for s, z in zip(seqs, charges)
+        ]
+    )
+
+    ions = tokenizer.calculate_precursor_ions(seqs, charges)
+    torch.testing.assert_close(ions, expected)
+
+    tokens = tokenizer.tokenize(seqs)
+    ions = tokenizer.calculate_precursor_ions(tokens, charges)
+    torch.testing.assert_close(ions, expected)
 
 
 def test_fragment_ions():
     """Test ion calculations."""
     tokenizer = PeptideIonTokenizer()
-    ions = tokenizer.ions(["LESLIEK"], [1])[0]
+    ions = tokenizer.calculate_ions(["LESLIEK"], [1])[0]
     expected = torch.tensor(LESLIEK_PLUS_ONE)[:, :, None]
     torch.testing.assert_close(ions.fragments, expected, check_dtype=False)
 
-    ions = tokenizer.ions(["LESLIEK"], [2])[0]
+    ions = tokenizer.calculate_ions(["LESLIEK"], [2])[0]
     expected = torch.cat(
         [
             torch.tensor(LESLIEK_PLUS_ONE)[:, :, None],
@@ -166,11 +198,11 @@ def test_fragment_ions():
     )
     torch.testing.assert_close(ions.fragments, expected, check_dtype=False)
 
-    ions = tokenizer.ions(["LESLIEK/1"], None)[0]
+    ions = tokenizer.calculate_ions(["LESLIEK/1"], None)[0]
     expected = torch.tensor(LESLIEK_PLUS_ONE)[:, :, None]
     torch.testing.assert_close(ions.fragments, expected, check_dtype=False)
 
-    ions = tokenizer.ions(["LESLIEK/3"], None)[0]
+    ions = tokenizer.calculate_ions(["LESLIEK/3"], None)[0]
     expected = torch.cat(
         [
             torch.tensor(LESLIEK_PLUS_ONE)[:, :, None],
@@ -181,7 +213,7 @@ def test_fragment_ions():
     torch.testing.assert_close(ions.fragments, expected, check_dtype=False)
 
     tokenizer = PeptideIonTokenizer.from_proforma(["[+10]-LESLIEK"])
-    ions = tokenizer.ions(["[+10.000000]-LESLIEK"], 1)[0]
+    ions = tokenizer.calculate_ions(["[+10.000000]-LESLIEK"], 1)[0]
     expected = torch.tensor(LESLIEK_PLUS_ONE)[:, :, None]
     expected[0, :, :] += 10
     torch.testing.assert_close(ions.fragments, expected, check_dtype=False)